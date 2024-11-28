77b3e6bf668662855e33f8d2247b8120cb546b3e
Tobias Høyrup Hemmingsen <tohoy@dtu.dk>
2024-11-28 15:54:49 +0100

    Create interactive-volvo.py
    Created the file with the overall structure

---
+++ volvo/interactive-volvo.py
@@ -0,0 +1,19 @@
+"""
+This scripts presents an interactive overview of the Volvo thermal evaporation chamber and the attached instruments
+
+Created by Tobias H. Hemmingsen (tohoy)
+Created on 28-11-2024
+"""
+
+# Import libraries
+import tkinter as tk
+
+
+
+# Make a sketch of the Volvo
+
+
+# Make detailed sketches of the instruments
+
+
+# Add the ability to click on instruments to view more
